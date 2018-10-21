98f75fdf079c6cf1703e81df55112c0b4adf3250
vrajeshpatel4u <vrajeshpatel4u@gmail.com>
2018-10-21 11:08:26 +0530

    Magento2 Sample Theme

---
+++ app/design/frontend/V4U/Sampletheme/registration.php
@@ -0,0 +1,8 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/V4U/sampletheme',
+    __DIR__
+);
+?>
